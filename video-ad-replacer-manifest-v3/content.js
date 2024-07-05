416533e7e8df561dbbdbafd9428612e4e57b5230
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2024-07-05 16:54:43 -0600

    Add automatic skipping

--- video-ad-replacer-manifest-v3/content.js
+++ video-ad-replacer-manifest-v3/content.js
@@ -1,6 +1,7 @@
 /*
     @source: https://github.com/jacob-willden/video-ad-replacer/
     @source: https://github.com/rdp/sensible-cinema/
+    @source: https://github.com/tientq64/userscripts/blob/main/scripts/auto-skip-youtube-ads/script.user.js
 
     @licstart  The following is the entire license notice for the
     code in this page.
@@ -10,18 +11,25 @@
     Video Ad Replacer Extension Project Copyright (C) 2021 Jacob Willden
     (Released under the GNU General Public License (GNU GPL) Version 3.0 or later)
 
-    Sensible Cinema (Play It My Way) Source Code Copyright (C) 2016, 2017, 2018 Roger Pack 
+    Sensible Cinema (Play It My Way) Source Code Copyright (C) 2016, 2017, 2018 Roger Pack
     (Released under the Lesser General Public License (LGPL))
 
+    Auto Skip YouTube Ads User Script Copyright (C) 2024 tientq64 (Tran Quang Tien)
+    (Released under the MIT License (Expat Variation))
+
     Some of the code below is modified from the "edited_generic_player.js"
-    source code file in the "chrome_extension" folder in the 
-    "html5_javascript" folder from the Sensible Cinema (Play It My Way) 
+    source code file in the "chrome_extension" folder in the
+    "html5_javascript" folder from the Sensible Cinema (Play It My Way)
     repository (source link above), and it is explicitly labled as so.
 
-    Afformentioned source code derived and modified by Jacob Willden
-    Date of Derivation/Modification: November 5, 2021
+    Some code below is derived from the "script.user.js" source code file
+    linked above, and is explicitly labeled as so.
 
-    The Video Ad Replacer Extension Project is free software: 
+    Afformentioned source code derived and modified by Jacob Willden
+    Inital Date of Derivation/Modification: November 5, 2021
+    Latest Date of Derivation/Modification: July 5, 2024
+
+    The Video Ad Replacer Extension Project is free software:
     you can redistribute it and/or modify it under the terms of the GNU
     General Public License (GNU GPL) as published by the Free Software
     Foundation, either version 3 of the License, or (at your option)
@@ -38,7 +46,7 @@
     You should have recieved a copy of the GNU General Public License
     along with this project. Otherwise, see: https://www.gnu.org/licenses/
 
-    @licend  The above is the entire license notice for the 
+    @licend  The above is the entire license notice for the
     code in this page.
 */
 
@@ -59,6 +67,27 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
+/* Function derived and modified from "script.user.js" from Auto Skip
+YouTube Ads, which is under the MIT License (Expat Varation):
+
+Copyright (c) 2024 tientq64
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+function skipIfPossible() {
+    const skipButton = document.querySelector(`
+        .ytp-skip-ad-button,
+        .ytp-ad-skip-button,
+        .ytp-ad-skip-button-modern
+    `);
+    if(skipButton) {
+        skipButton.click();
+    }
+}
+
 // Function derived and modified from "edited_generic_player.js" from Sensible Cinema (checkStatus)
 function checkForAdvertisement() {
     var adIndicator = document.querySelector('.ytp-ad-player-overlay');
@@ -68,6 +97,9 @@ function checkForAdvertisement() {
             setForAdvertisement = true;
             myVideo.muted = true;
             myVideo.style.opacity = 0;
+        }
+        else {
+            skipIfPossible();
         }
     }
     else { // Ad is over now or the extension was deactivated
@@ -82,5 +114,7 @@ function checkForAdvertisement() {
 
 setInterval(checkForAdvertisement, 10); // Keep interval going in case there's another ad
 
-// Figure out how to include images, patterns, etc, in both the extension and the user script
+// Check if copyright-claimed and if so, add dialog element to notify users?
+
+// Figure out how to include images, patterns, etc, in both the extension and the user script?
 // Add promise error handling?
